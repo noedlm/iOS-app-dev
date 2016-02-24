f9effb5337df114fca1c5c56ef2dd52e8111808b
Noe De La Mora <noedelamora@gmail.com>
2016-02-23 21:16:44 -0600

    initial commit for project

---
+++ PrimerGit/PrimerGit/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PrimerGit
+//
+//  Created by Angel De La Mora on 2/23/16.
+//  Copyright © 2016 Noe Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
